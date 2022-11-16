c65df1af31660a0c6ffff86ceab5bce2e7eaabb5
Mike Lehmann <mr.lehmann.michael@gmail.com>
2022-11-15 23:00:37 -0800

    added training_images to digit_matrix

--- Naive_Bayes_ML.m
+++ Naive_Bayes_ML.m
@@ -18,8 +18,11 @@ test_digits = 10000;
 % Create instance vector for each digit label
 digit_labels = zeros(1,10);
 
+% Get train_labels size for loops
+labels_size = size(train_labels,1);
+
 % Loop through train_labels to calculate how many per digit
-for i=1:size(train_labels,1)
+for i=1:labels_size
     number_value = train_labels(i); % Grab digit label number
     digit_labels(number_value+1) = digit_labels(number_value+1) + 1; % Increment instance
 end
@@ -27,7 +30,29 @@ end
 % digit_labels/50000 * 100
 % Create matrix for each vector from 0-9 to hold cumulative training data
 digit_matrix = zeros(size(train_imgs,1)*size(train_imgs,2),10);
-example_img = train_imgs(:,:,1); % Extract image (20x20 array)
-example_img(example_img > 0) = 1; % Set values greater than 0 to 1
+% example_img = train_imgs(:,:,1); % Extract image (20x20 matrix)
+% example_img(example_img >= 0.5) = 1; % Set values greater than 0.5 to 1
+% example_img(example_img < 0.5) = 0;
+% example_vec = example_img(:); % Set 20x20 image as single vector
+
+% Loop through train_imgs to fill in digit_matrix
+for i=1:labels_size
+    num = train_labels(i); % Grab number value
+    img = train_imgs(:,:,i); % Grab image
+    img(img >= 0.5) = 1; % Set to either 0 or 1
+    img(img < 0.5) = 0;
+    vec = img(:); % Get as a vector
+    % Sum vector values into single matrix
+    for j=1:size(vec,1)
+        digit_matrix(j,num+1) = digit_matrix(j,num+1) + vec(j);
+    end
+end
+% Normalize matrix?
+for i= 1:10
+    digit_matrix(:,i)=digit_matrix(:,i)/digit_labels(i);
+end
+
+% How to do a heatmap/colormap for each digit?
+
 % Pull in test data
 %[test_imgs test_labels] = readMNIST('t10k-images-idx3-ubyte/t10k-images.idx3-ubyte', 't10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte', test_digits, 0);
