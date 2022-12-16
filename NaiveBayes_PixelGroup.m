f257267bd69fdcd348859df152359ea25888dc2a
aleckm <alec.k.mor@gmail.com>
2022-12-16 01:36:03 -0800

    progres on test image import

--- NaiveBayes_PixelGroup.m
+++ NaiveBayes_PixelGroup.m
@@ -20,15 +20,14 @@
 % 6.Laplace smooth - for the count of each feature in each location for
 % each digit add k (could be 1). For the count of training examples from
 % this class add k*V (V is the number of possible values of the feature)
-% 7.Calculate the 
-% 8.Load in Test data
+% 7.Load in Test data
 
 clear;
 close all;
 clc;
 
-train_digits = uint16(60000);
-test_digits = uint16(10000);
+train_digits = 60000;
+test_digits = 10000;
 
 
 % Pull in training data
@@ -38,7 +37,7 @@ test_digits = uint16(10000);
 digit_labels = zeros(1,10);
 
 % Get train_labels size for loops
-labels_size = uint16(size(train_labels,1));
+labels_size = size(train_labels,1);
 
 % Create matrix for each vector from 0-9 to hold cumulative training data
 digit_matrix = zeros(size(train_imgs,1)*size(train_imgs,2),10);
@@ -78,7 +77,8 @@ for i = 1:labels_size
     %vec = img(:); % Get as a vector
     labelG = train_labels(i); %grab what the label of this training digit is
     digit_labels(labelG+1) = digit_labels(labelG+1) + 1; %Add to count of the total of this digit in the training data
-    for j = 1:size(train_imgs,1)-1
+    for j = 1:size(train_imgs,1)-1 %The groups are overlapping. The last pixel location of each row does not have a pixel group due to the 2X2 size
+
         for k= 1:size(train_imgs,2)-1
             Gij = [img(j,k), img(j+1,k), img(j,k+1), img(j+1,k+1)];
             %trainG(j,k,i,:) = Gij; %make this faster? takes 28 seconds
@@ -108,6 +108,47 @@ heatmap(lsmooth(:,:,1,1));
 % Pull in test data
 [test_imgs, test_labels] = readMNIST('t10k-images-idx3-ubyte/t10k-images.idx3-ubyte', 't10k-labels-idx1-ubyte/t10k-labels.idx1-ubyte', test_digits, 0);
 
+%Size of the 
+test_labels_size = size(test_labels, 1);
+% How many of each digit in the test images
+test_digit_labels = zeros(1,10);
+
+%%where I left off
+
+% Create matrix for each vector from 0-9 to hold cumulative training data
+digit_matrix = zeros(size(train_imgs,1)*size(train_imgs,2),10);
+
+%make picture a matrix of Gij
+%2x2 overlapping
+
+%Initialze matrix. 4-D matrix.
+%First two parameters are i,j coordinates of pixel group feature
+%Third parameter is the index of the training data (nth image)
+%Fourth parameter is the index of the pixel group feature
+%trainG = zeros(size(train_imgs,1)-1,size(train_imgs,2)-1, labels_size, 4); 
+
+%Initialize 3-D matrix. Each layer is a 2D matrix. Each entry in that
+%matrix is the pixel group ID for that region of the image. This is done
+%for each image in the training data
+imgG = zeros(size(train_imgs,1)-1,size(train_imgs,2)-1, labels_size); 
+
+%Number of individual features in the transformed image
+lenG = (size(train_imgs,1)-1)*(size(train_imgs,2)-1);
+
+%Matrix holding count of each group value for each location in the image
+%for each digit
+countG = zeros((size(train_imgs,1)-1), (size(train_imgs,2)-1), 16, 10);
+
+%Vector that represents an individual feature. This will be turned into a
+%number fore simplicity.
+Gij = zeros(4,1);
+
+sum = 0;
+labelG = 0;
+
+% Create confusion matrix
+confusion = zeros(10,10);
+error = 0;
 
 % 
 % % Loop through train_labels to calculate how many per digit
